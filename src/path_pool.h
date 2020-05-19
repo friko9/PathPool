bb2a498b4819d16d5733e05cb2c4b2cf8ebe67d8
friko9 <friko9@gmail.com>
2020-05-20 00:55:29 +0200

    feat[PathPoolHashmap] done basic functionality

--- src/path_pool.h
+++ src/path_pool.h
@@ -1,55 +1,64 @@
 #ifndef PATH_POOL_H
 #define PATH_POOL_H
 
+#include "./field_ref_iterator.h"
+
 #include <boost/flyweight.hpp>
 #include <unordered_map>
 #include <string>
 #include <vector>
 
-
-template <typename T,typename... tail>
-inline
-T make(tail... args)
-{
-  return T{args...};
-}
-
-template<>
-class std::hash<boost::flyweight<std::string>>
-{
-public:
-  size_t operator()(boost::flyweight<std::string> arg) const noexcept
-  {
-    using hashed_t = std::decay<decltype(arg.get_key())>::type;
-    return std::hash<hashed_t>{}(arg.get_key());
-  }
-};
-
-
 class PathPoolHashmap
 {
 public:
-  using index_t = size_t;
-  using key_t = boost::flyweight<std::string>;
+  using key_t = size_t;
+  using tag_t = boost::flyweight<std::string>;
 private:
-  struct node_t
+  struct node_t : public std::unordered_map<tag_t,key_t>
   {
-    node_t() = default;
-    node_t(const node_t&) = default;
-    node_t(const std::unordered_map<key_t,index_t>& children,index_t parent):
-      m_children{children},m_parent{parent}
+    node_t(const std::unordered_map<tag_t,key_t>& children, tag_t name, key_t parent):
+      std::unordered_map<tag_t,key_t>{children},
+      m_parent{parent},
+      m_name{name}
     {}
-    std::unordered_map<key_t,index_t> m_children;
-    index_t m_parent;
+    key_t m_parent;
+    tag_t m_name;
   };
 public:
-  void f()
+  key_t get_subnode(key_t path,tag_t subnode)
   {
-    x.get_key();
+    auto result = nodes[path].emplace(subnode,nodes.size());
+    if(result.second)
+      nodes.push_back(node_t{{},subnode,path});
+    return result.first->second;
+  }
+  key_t get_parent(key_t path)
+  {
+    return nodes[path].m_parent;
+  }
+  std::vector<key_t> get_siblings(key_t path)
+  {
+    std::vector<key_t> result;
+    auto value_t_ref = &std::decay<decltype(*nodes[path].begin())>::type::second;
+    std::copy(field_ref(nodes[path].begin(),value_t_ref),
+	      field_ref(nodes[path].end(),value_t_ref),
+	      std::back_inserter(result));
+    return result;
+  }
+  tag_t get_tagname(key_t path)
+  {
+    return nodes[path].m_name;
+  }
+  std::vector<tag_t> get_pathname(key_t path)
+  {
+    std::vector<tag_t> result;  
+    for(; path != 0; path = nodes[path].m_parent)
+      result.push_back(get_tagname(path));
+    return result;
   }
 private:
-  std::unordered_map<key_t,node_t> nodes {{key_t{"/"},node_t{{},index_t{0}}}};
-  key_t x;
+  std::vector<node_t> nodes {{node_t{{},tag_t{""},key_t{0}}}};
+  tag_t x;
 };
 
 #endif /* PATH_POOL_H */
