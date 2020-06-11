a67659122055d6ef0db2040cb7a633f0cd78637d
friko9 <friko9@gmail.com>
2020-06-11 02:15:26 +0200

    build[coverage] minor changes [fix] change gcovr to lcovr and add its dummy def [fix] remove CodeCoverage warrning

--- cmake_scripts/Coverage.cmake
+++ cmake_scripts/Coverage.cmake
@@ -1,8 +1,11 @@
 if (CMAKE_BUILD_TYPE STREQUAL "Coverage")
+  set(CMAKE_BUILD_TYPE "Debug")
   include(CodeCoverage)
   SET(CMAKE_CXX_FLAGS "-g -O0 -fprofile-arcs -ftest-coverage")
   SET(CMAKE_C_FLAGS "-g -O0 -fprofile-arcs -ftest-coverage")
 else()
   function (setup_target_for_coverage_gcovr_html)
+  endfunction()
+  function (setup_target_for_coverage_lcov)
   endfunction()
 endif() #CMAKE_BUILD_TYPE STREQUAL "Coverage"
