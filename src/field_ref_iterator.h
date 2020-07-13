ed61d8941d07f863980565318afe27aebb193247
friko9 <friko9@gmail.com>
2020-07-14 01:29:11 +0200

    feat[morphed_iterator] iterator calling function on derefference

--- src/field_ref_iterator.h
+++ src/field_ref_iterator.h
@@ -23,6 +23,28 @@ field_ref_iterator<It_t,FieldRefT> field_ref(It_t it, FieldRefT ref)
   return field_ref_iterator<It_t,FieldRefT>{it,ref};
 }
 
+template<typename It_t,typename FuncT>
+struct morphed_iterator : It_t
+{
+  morphed_iterator(It_t it,FuncT func):
+    It_t{it},m_func{func}
+  {}
+
+  auto
+  operator *()
+  {
+    return this->m_func(this->It_t::operator*());
+  }
+private:
+  FuncT m_func;
+};
+
+template<typename It_t,typename FuncT>
+morphed_iterator<It_t,FuncT> morphed(It_t it, FuncT ref)
+{
+  return morphed_iterator<It_t,FuncT>{it,ref};
+}
+
 template <template<typename... X> class T,typename... tail>
 inline
 T<tail...> make(tail... args)
