2ea980b45a46e3bd1472863864a59abcbb1c3579
friko9 <friko9@gmail.com>
2020-07-25 21:03:48 +0200

    ref[impl_path_utils] remove template dependencies on the code
    ref[operator <] exclude template-parameter independent part to
    operator_less_impl

--- src/impl/impl_path_utils.h
+++ src/impl/impl_path_utils.h
@@ -18,15 +18,21 @@ bool operator ==(const Path<PoolT,poolno>& l, const Path<PoolT,poolno>& r) noexc
   return l.m_path == r.m_path;
 }
 
-template<typename PoolT,int poolno>
-bool operator <(const Path<PoolT,poolno>& l, const Path<PoolT,poolno>& r)
+template<typename PoolT>
+bool operator_less_impl(typename PoolT::pathid_t l, typename PoolT::pathid_t r, PoolT const& pool)
 {
-  using PathT = Path<PoolT,poolno>;
-  auto split = get_common_path(l.m_path,r.m_path,PathT::s_pool);
+  auto split = get_common_path(l, r, pool);
   if(split[1] == split[2]) return false;
   if(split[0] == split[1]) return true;
   if(split[0] == split[2]) return false;
-  return PathT::s_pool.get_tag(split[1]) < PathT::s_pool.get_tag(split[2]);
+  return pool.get_tag(split[1]) < pool.get_tag(split[2]);
+}
+
+template<typename PoolT,int poolno>
+inline
+bool operator <(const Path<PoolT,poolno>& l, const Path<PoolT,poolno>& r)
+{
+  return operator_less_impl(l.m_path, r.m_path, Path<PoolT,poolno>::s_pool);
 }
 
 #endif /* IMPL_PATH_UTILS_H */
